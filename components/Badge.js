87bb299ccc3539fb187a1d552da2a6cd5266046a
Asaad Saad <asaad@outlook.com>
2022-05-09 20:28:55 -0500

    Update Badge.js

--- components/Badge.js
+++ components/Badge.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+// Don't change this file
 
 const Badge = ({ userInfo }) => {
     const { avatar_url, name, login } = userInfo;
